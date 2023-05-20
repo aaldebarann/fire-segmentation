27ceeb3f5dbac06269e20bd8da163204e055bea0
Ivanchenko Aleksei <amivanchenko@yandex.ru>
2023-05-20 13:31:03 +0300

    Улучшена производительность сегментации области

--- utils/process.py
+++ utils/process.py
@@ -3,16 +3,22 @@ from train.prediction import predict
 import numpy as np
 import os
 from PIL import Image
+import math
 
 
 DEFAULT_MODEL = "model-resnet34-adam-0008.h5"
-MIN_LONGITUDE_DELTA = 0.4
+MIN_LONGITUDE_DELTA = 0.36
 MIN_LATITUDE_DELTA = 0.2
-DEPTH = 1
 
 
 def date_to_interval(date):
     return "{0}T00:00:00Z".format(date), "{0}T23:59:59Z".format(date)
+
+
+def request_size(bbox):
+    size = math.ceil((bbox[2] - bbox[0]) / MIN_LONGITUDE_DELTA)
+    size *= 256
+    return size
 
 
 def get_mask(bbox, time_interval, model=DEFAULT_MODEL):
@@ -21,56 +27,19 @@ def get_mask(bbox, time_interval, model=DEFAULT_MODEL):
     date is a string in format "yyyy-mm-dd"
     where y - year, m - month, d - day
     """
-    if bbox[2] - bbox[0] > MIN_LONGITUDE_DELTA:
-        im1 = get_mask(
-            [bbox[0],
-             bbox[1],
-             (bbox[0] + bbox[2]) / 2,
-             bbox[3],
-             ],
-            model,
-            time_interval)
-        im2 = get_mask(
-            [(bbox[0] + bbox[2]) / 2,
-             bbox[1],
-             bbox[2],
-             bbox[3],
-             ],
-            model,
-            time_interval)
-        return np.concatenate((im1, im2), axis=1)
-    elif bbox[3] - bbox[1] > MIN_LATITUDE_DELTA:
-        im1 = get_mask(
-            [bbox[0],
-             bbox[1],
-             bbox[2],
-             (bbox[3] + bbox[1])/2,
-             ],
-            model,
-            time_interval)
-        im2 = get_mask(
-            [bbox[0],
-             (bbox[3] + bbox[1])/2,
-             bbox[2],
-             bbox[3],
-             ],
-            model,
-            time_interval)
-        return np.concatenate((im2, im1), axis=0)
-    else:
-        x = download(bbox, time_interval, rescale=True)
-        batch = np.array([x, ])
-        y = predict(batch, model)[0]
-        return y
+    x = get_image(bbox, time_interval, rescale=True, type=np.float32)
+    y = get_valid_area_mask(x, model)
+    
+    return y
 
 
-def get_image(bbox, time_interval):
+def get_image(bbox, time_interval, rescale=False, type=np.uint8):
     assert (bbox[2] > bbox[0] and bbox[3] > bbox[1])
     """
     date is a string in format "yyyy-mm-dd"
     where y - year, m - month, d - day
     """
-    if bbox[2] - bbox[0] > MIN_LONGITUDE_DELTA:
+    if bbox[2] - bbox[0] > MIN_LONGITUDE_DELTA * 9:
         im1 = get_image(
             [bbox[0],
              bbox[1],
@@ -86,7 +55,7 @@ def get_image(bbox, time_interval):
              ],
             time_interval)
         return np.concatenate((im1, im2), axis=1)
-    elif bbox[3] - bbox[1] > MIN_LATITUDE_DELTA:
+    elif bbox[3] - bbox[1] > MIN_LATITUDE_DELTA * 9:
         im1 = get_image(
             [bbox[0],
              bbox[1],
@@ -103,61 +72,27 @@ def get_image(bbox, time_interval):
             time_interval)
         return np.concatenate((im2, im1), axis=0)
     else:
-        x = download(bbox, time_interval, rescale=False)
-        x = x.astype(np.uint8)
+        size = request_size(bbox)
+        x = download(bbox, time_interval, rescale=rescale, width=size, height=size)
+        x = x.astype(type)
         return x
+        
+
+def split(im, width = 256, heigth = 256):
+    M = width
+    N = heigth
+    tiles = [im[u:u+M,v:v+N] for u in range(0,im.shape[0],M) for v in range(0,im.shape[1],N)]
+    return np.asarray(tiles)
 
 
-def get_valid_area_image(depth, bbox, time_interval):
-    x = download(bbox, time_interval, width=256*(depth+1), height=256*(depth+1))
-    x = x.astype(np.uint8)
-    return x
+def get_valid_area_mask(x, model):
+    batch = split(x)
+    mask_batch = predict(batch, model)
+    mask = np.empty((x.shape[0], x.shape[1], 1))
+    N = mask_batch.shape[0] // 2
 
+    for i in range(0, N, 256):
+        for j in range(0, N, 256):
+            mask[i:i+256, j:j+256] = mask_batch[(i//256)*N + (j//256)]
 
-def split_image(img):
-    # splits image into 4 pieces
-
-    xs = img.shape[0] // 2  # division lines for the picture
-    ys = img.shape[1] // 2
-    # now slice up the image (in a shape that works well with subplots)
-    splits = [img[0:xs, 0:ys], img[0:xs, ys:], img[xs:, 0:ys], img[xs:, ys:]]
-    return splits
-
-
-def join_images(ul, ur, dl, dr):
-    u = np.concatenate((ul, ur), axis=1)
-    d = np.concatenate((dl, dr), axis=1)
-    image = np.concatenate((u, d), axis=0)
-    return image
-
-
-def get_valid_area_mask(depth, x, rescale=True, model=DEFAULT_MODEL):
-    batch = np.array(split_image(x))
-    batch2 = list()
-    for x in batch:
-        split = split_image(x)
-        for y in split:
-            batch2.append(y)
-
-    masks2 = predict(np.array(batch2), model)
-    masks = list()
-    for i in range(4):
-        x = join_images(masks2[i*4], masks2[i*4+1], masks2[i*4+2], masks2[i*4+3])
-        masks.append(x)
-    masks = np.array(masks)
-    mask = join_images(masks[0], masks[1], masks[2], masks[3])
     return mask
-
-    """
-    if depth > 0:
-        masks = list()
-        for img in batch:
-            masks.append(get_valid_area_mask(depth-1, img, rescale, model))
-        mask = join_images(masks[0], masks[1], masks[2], masks[3])
-        return mask
-    else:
-        masks = predict(batch, model)
-        mask = join_images(masks[0], masks[1], masks[2], masks[3])
-        return mask
-"""
-
